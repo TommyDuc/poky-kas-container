c3a728e552bba459048cc1c5ee909f2c26f0de4d
Raymond Gauthier <jraygauthier@gmail.com>
2024-07-05 16:30:38 -0400

    sh: remove old scripts
    Ref: ASP00066-2003

--- build_image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-DISTRO_CROPS_POKY=${1:-ubuntu-18.04}
-KAS_VERSION=${2:-2.6.2}
-
-docker build --build-arg DISTRO_CROPS_POKY=${DISTRO_CROPS_POKY} --build-arg KAS_VERSION=${KAS_VERSION} -t dimonoff/poky-kas-container:${DISTRO_CROPS_POKY}-kas-${KAS_VERSION} .
--- deploy_image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-DISTRO_CROPS_POKY=${1:-ubuntu-18.04}
-KAS_VERSION=${2:-2.6.2}
-
-./build_image.sh $1 $2
-
-docker push dimonoff/poky-kas-container:${DISTRO_CROPS_POKY}-kas-${KAS_VERSION}
